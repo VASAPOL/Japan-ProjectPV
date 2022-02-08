889999c2dbe0c2b077fee6491cda475a5d0ff632
Vasapol <Developer.vasapol@gmail.com>
2022-02-08 21:50:54 +0700

    Initial Commit

---
+++ Japan Project/Japan_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Japan_ProjectApp.swift
+//  Japan Project
+//
+//  Created by Vasapol on 8/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Japan_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Japan ProjectUITests/Japan_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Japan_ProjectUITestsLaunchTests.swift
+//  Japan ProjectUITests
+//
+//  Created by Vasapol on 8/2/2565 BE.
+//
+
+import XCTest
+
+class Japan_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
