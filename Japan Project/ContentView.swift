4fe3fc53587d428aa37ebfe2d983db0aa344b057
Vasapol <Developer.vasapol@gmail.com>
2022-02-09 13:08:45 +0700

    t

--- Japan Project/ContentView.swift
+++ Japan Project/ContentView.swift
@@ -14,12 +14,67 @@ struct ContentView: View {
     @State var textx: String = "0"
     @State var supply = Int(Type_Input)/10
     var body: some View {
+        NavigationView{
+            VStack{
+                ZStack(alignment: .leading){
+                    Rectangle()
+                        .frame(width: 500, height: 250)
+                        .foregroundColor(.blue)
+                        .ignoresSafeArea()
+                    RoundedRectangle(cornerRadius: 20)
+                        .padding(.horizontal, 60.0)
+                        .foregroundColor(.white)
+                        .frame(height: 100)
+                    HStack{
+                        Spacer(minLength:70)
+                        PictureView(picname: "Paper_Image", LableDown: "Paper")
+                            .padding(.horizontal, 2)
+                        PictureView(picname: "Paper_Image", LableDown: "Paper")
+                            .padding(.horizontal, 2)
+                        PictureView(picname: "Paper_Image", LableDown: "Paper")
+                            .padding(.horizontal, 2)
+                        PictureView(picname: "Paper_Image", LableDown: "Paper")
+                            .padding(.horizontal, 2)
+                        PictureView(picname: "Paper_Image", LableDown: "Paper")
+                            .padding(.horizontal, 2)
+                        Spacer(minLength: 70)
+                    }
+                }
+                Spacer()
+            }.hiddenNavigationBarStyle()
+        }.navigationBarHidden(true)
+    }
+}
+
+struct PictureView:View{
+    var picname: String
+    var LableDown: String
+    var body: some View{
         HStack{
-            Text("d")
-            //.
+            Spacer()
+            Button(action:{
+                
+            }){
+                VStack{
+                    Image(picname)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .cornerRadius(50)
+                        .foregroundColor(Color.blue)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 50)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                        Text(LableDown)
+                        .foregroundColor(Color.black)
+                }
+                Spacer()
+                
+            }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Japan Project/View_Modifier.swift
@@ -0,0 +1,23 @@
+//
+//  View_Modifier.swift
+//  Japan Project
+//
+//  Created by Vasapol on 9/2/2565 BE.
+//
+
+
+import SwiftUI
+
+struct HiddenNavigationBar: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+        .navigationBarTitle("", displayMode: .inline)
+        .navigationBarHidden(true)
+    }
+}
+
+extension View {
+    func hiddenNavigationBarStyle() -> some View {
+        modifier( HiddenNavigationBar() )
+    }
+}
